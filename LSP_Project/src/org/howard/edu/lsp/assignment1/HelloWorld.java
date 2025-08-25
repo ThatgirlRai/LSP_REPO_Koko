6b9971db335664c0331eac5c9ba403c88e2bc584
ThatgirlRai <sarai.prentice@bison.howard.edu>
2025-08-25 16:17:54 -0400

    correction to assignment 1

--- LSP_Project/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ LSP_Project/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,11 +1,11 @@
-/**
- * Name: Sarai Prentice
- */
+
 
 package org.howard.edu.lsp.assignment1;
 
 public class HelloWorld {
-
+/**
+ * Name: Sarai Prentice
+ */
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
 
